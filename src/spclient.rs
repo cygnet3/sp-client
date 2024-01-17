0cc3b770b2ce14b4ac52db9b421d2bd5ab0920f3
Sosthene00 <674694@protonmail.ch>
2024-01-17 13:39:13 +0100

    Add create_psbt api

--- src/spclient.rs
+++ src/spclient.rs
@@ -1,15 +1,46 @@
-use bip39::Mnemonic;
-use bitcoin::{bip32::{DerivationPath, Xpriv}, secp256k1::{constants::SECRET_KEY_SIZE, PublicKey, Secp256k1, SecretKey}, Network};
+use std::{
+    collections::{BTreeMap, HashMap},
+    str::FromStr,
+};
+
+use bip39::{
+    rand::{self, seq::SliceRandom},
+    Mnemonic,
+};
+
+use bitcoin::psbt::{raw, Input, Output};
+use bitcoin::{
+    bip32::{DerivationPath, Xpriv},
+    consensus::{deserialize, serialize},
+    hashes::hex::FromHex,
+    key::TapTweak,
+    psbt::PsbtSighashType,
+    secp256k1::{
+        constants::SECRET_KEY_SIZE, Keypair, Message, PublicKey, Scalar, Secp256k1, SecretKey,
+        ThirtyTwoByteHash,
+    },
+    sighash::{Prevouts, SighashCache},
+    taproot::Signature,
+    Address, Amount, Network, ScriptBuf, TapLeafHash, Transaction, TxIn, TxOut, Witness,
+};
 use nakamoto::common::bitcoin::OutPoint;
-use serde::{Serialize, Deserialize};
+
+use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use serde_with::DisplayFromStr;
+
 use silentpayments::receiving::Receiver;
-use std::{str::FromStr, collections::HashMap};
+use silentpayments::sending::SilentPaymentAddress;
+use silentpayments::utils as sp_utils;
 
-use anyhow::Result;
+use anyhow::{Error, Result};
 
+use crate::constants::{
+    NUMS, PSBT_SP_ADDRESS_KEY, PSBT_SP_PREFIX, PSBT_SP_SUBTYPE, PSBT_SP_TWEAK_KEY,
+};
 use crate::{db::FileWriter, stream::loginfo};
+
+pub use bitcoin::psbt::Psbt;
 
 pub struct ScanProgress {
     pub start: u32,
@@ -26,6 +57,13 @@ pub struct OwnedOutput {
     pub script: String,
     pub spent: bool,
     pub spent_by: Option<String>
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct Recipient {
+    pub address: String, // either old school or silent payment
+    pub amount: u64,
+    pub nb_outputs: u32 // if address is not SP, only 1 is valid
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
@@ -163,6 +201,110 @@ impl SpClient {
     pub fn get_scan_key(&self) -> SecretKey {
         self.scan_sk.clone()
     }
+
+    pub fn create_new_psbt(inputs: Vec<OwnedOutput>, recipients: Vec<Recipient>) -> Result<Psbt> {
+        let mut network: Option<Network> = None;
+        let mut tx_in: Vec<bitcoin::TxIn> = vec![];
+        let mut inputs_data: Vec<(ScriptBuf, u64, Scalar)> = vec![];
+
+        for i in inputs {
+            tx_in.push(TxIn { 
+                previous_output: bitcoin::OutPoint::from_str(&i.txoutpoint)?,
+                script_sig: ScriptBuf::new(), 
+                sequence: bitcoin::Sequence::MAX, 
+                witness: bitcoin::Witness::new()
+            });
+
+            let scalar = Scalar::from_be_bytes(FromHex::from_hex(&i.tweak)?)?;
+
+            inputs_data.push((ScriptBuf::from_hex(&i.script)?, i.amount, scalar));
+        }
+
+        // Since we don't have access to private materials for now we use a NUMS key as a placeholder 
+        let placeholder_key = bitcoin::XOnlyPublicKey::from_str(NUMS)?.dangerous_assume_tweaked();
+
+        let _outputs: Result<Vec<bitcoin::TxOut>> = recipients.iter()
+            .map(|o| {
+                let address: Address;
+
+                match SilentPaymentAddress::try_from(o.address.as_str()) {
+                    Ok(sp_address) => {
+                        let address_network = if sp_address.is_testnet() { Network::Testnet } else { Network::Bitcoin };
+
+                        if let Some(network) = network {
+                            if network != address_network { return Err(Error::msg(format!("Wrong network for address {}", sp_address)))}
+                        } else {
+                            network = Some(address_network);
+                        }
+
+                        address = Address::from_script(&ScriptBuf::new_p2tr_tweaked(placeholder_key), address_network).unwrap();
+                    },
+                    Err(_) => {
+                        let unchecked_address = Address::from_str(&o.address)?; // TODO: handle better garbage string
+
+                        if let Some(network) = network {
+                            if network != *unchecked_address.network() { return Err(Error::msg(format!("Wrong network for address {}", unchecked_address.assume_checked())))}
+                        } else {
+                            network = Some(*unchecked_address.network());
+                        }
+
+                        address = unchecked_address.assume_checked();
+                    },
+                }
+
+                Ok(TxOut {
+                    value: Amount::from_sat(o.amount),
+                    script_pubkey: address.script_pubkey()
+                })
+            })
+            .collect();
+
+        let outputs = _outputs?;
+
+        let tx = bitcoin::Transaction {
+            version: bitcoin::transaction::Version(2),
+            lock_time: bitcoin::absolute::LockTime::ZERO,
+            input: tx_in,
+            output: outputs
+        };
+
+        let mut psbt = Psbt::from_unsigned_tx(tx)?;
+
+        // Add the witness utxo to the input in psbt
+        for (i, input_data) in inputs_data.iter().enumerate() {
+            let (script_pubkey, value, tweak) = input_data;
+            let witness_txout = TxOut {
+                value: Amount::from_sat(*value),
+                script_pubkey: script_pubkey.clone()
+            };
+            let mut psbt_input = Input { witness_utxo: Some(witness_txout), ..Default::default() };
+            psbt_input.proprietary.insert(raw::ProprietaryKey {
+                prefix: PSBT_SP_PREFIX.as_bytes().to_vec(),
+                subtype: PSBT_SP_SUBTYPE,
+                key: PSBT_SP_TWEAK_KEY.as_bytes().to_vec()
+            }, tweak.to_be_bytes().to_vec());
+            psbt.inputs[i] = psbt_input;
+        }
+        
+        for (i, recipient) in recipients.iter().enumerate() {
+            if let Ok(sp_address) = SilentPaymentAddress::try_from(recipient.address.as_str()) {
+                // Add silentpayment address to the output
+                let mut psbt_output = Output { ..Default::default() };
+                psbt_output.proprietary.insert(raw::ProprietaryKey {
+                    prefix: PSBT_SP_PREFIX.as_bytes().to_vec(),
+                    subtype: PSBT_SP_SUBTYPE,
+                    key: PSBT_SP_ADDRESS_KEY.as_bytes().to_vec()
+                }, serialize(&sp_address.to_string()));
+                psbt.outputs[i] = psbt_output;
+            } else {
+                // Regular address, we don't need to add more data
+                continue;
+            }
+        }
+
+        Ok(psbt)
+    }
+
 }
 
 pub fn derive_keys_from_mnemonic(
