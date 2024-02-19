be676a103ae2db7b404972e1794a8cf0adbb61c7
cygnet <cygnet3@proton.me>
2024-02-20 00:22:29 +0100

    Use silentpayments.dev domain name

--- src/electrumclient.rs
+++ src/electrumclient.rs
@@ -1,7 +1,7 @@
 use electrum_client::{Client, ConfigBuilder};
 use log::info;
 
-const ELECTRS_URI: &str = "ssl://170.75.163.219:51002";
+const ELECTRS_URI: &str = "ssl://silentpayments.dev:51002";
 const VALIDATE_DOMAIN: bool = false; // self-signed cert, so we don't validate
 
 pub fn create_electrum_client() -> anyhow::Result<Client> {
