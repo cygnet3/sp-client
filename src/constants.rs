c9af26fc4885a72c81250462e2352a29c88fc831
cygnet <cygnet3@proton.me>
2024-03-17 23:39:39 +0100

    Remove everything not needed for spclient

--- src/constants.rs
+++ src/constants.rs
@@ -1,19 +1,3 @@
-use log::LevelFilter;
-use serde::{Deserialize, Serialize};
-
-type SecretKeyString = String;
-type PublicKeyString = String;
-
-#[derive(Deserialize, Serialize)]
-pub enum WalletType {
-    New,
-    Mnemonic(String),
-    // scan_sk_hex, spend_sk_hex
-    PrivateKeys(SecretKeyString, SecretKeyString),
-    // scan_sk_hex, spend_pk_hex
-    ReadOnly(SecretKeyString, PublicKeyString),
-}
-
 pub const PSBT_SP_PREFIX: &str = "sp";
 pub const PSBT_SP_SUBTYPE: u8 = 0;
 pub const PSBT_SP_TWEAK_KEY: &str = "tweak";
@@ -25,36 +9,3 @@ pub const NUMS: &str = "50929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9a
 // If the change amount is below this number, we don't bother making a change address.
 // Instead, the funds will be added to the transaction fee.
 pub const DUST_THRESHOLD: u64 = 10_000;
-
-pub struct LogEntry {
-    pub time_millis: i64,
-    pub level: String,
-    pub tag: String,
-    pub msg: String,
-}
-
-pub enum LogLevel {
-    Debug,
-    Info,
-    Warn,
-    Error,
-    Off,
-}
-
-impl From<LogLevel> for LevelFilter {
-    fn from(value: LogLevel) -> Self {
-        match value {
-            LogLevel::Debug => LevelFilter::Debug,
-            LogLevel::Info => LevelFilter::Info,
-            LogLevel::Warn => LevelFilter::Warn,
-            LogLevel::Error => LevelFilter::Error,
-            LogLevel::Off => LevelFilter::Off,
-        }
-    }
-}
-
-pub struct SyncStatus {
-    pub peer_count: u32,
-    pub blockheight: u64,
-    pub bestblockhash: String,
-}
