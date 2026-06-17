7d71d15e8d089fb37513cf9944b61aaee32337a9
Sosthene <contact@sosthene.net>
2026-06-17 22:09:53 +0200

    refactor: adjust visibility in `hash.rs`, minor changes to `calculate_input_hash`

--- silentpayments/src/utils/hash.rs
+++ silentpayments/src/utils/hash.rs
@@ -1,15 +1,15 @@
-use crate::utils::common::{NonEmptyArray, OutPoint, SharedSecret};
+use crate::utils::common::{OutPoint, SharedSecret};
 use bitcoin_hashes::{Hash, HashEngine, sha256t_hash_newtype};
 use secp256k1::{PublicKey, Scalar, SecretKey};
 
 sha256t_hash_newtype! {
-    pub(crate) struct InputsTag = hash_str("BIP0352/Inputs");
+    struct InputsTag = hash_str("BIP0352/Inputs");
 
     /// BIP0352-tagged hash with tag \"Inputs\".
     ///
     /// This is used for computing the inputs hash.
     #[hash_newtype(forward)]
-    pub(crate) struct InputsHash(_);
+    struct InputsHash(_);
 
     pub(crate) struct LabelTag = hash_str("BIP0352/Label");
 
@@ -29,16 +29,14 @@ sha256t_hash_newtype! {
 }
 
 impl InputsHash {
-    pub(crate) fn from_outpoint_and_A_sum(
-        smallest_outpoint: &OutPoint,
-        A_sum: PublicKey,
-    ) -> InputsHash {
+    fn from_outpoint_and_A_sum(smallest_outpoint: &OutPoint, A_sum: PublicKey) -> InputsHash {
         let mut eng = InputsHash::engine();
         eng.input(&smallest_outpoint.0);
         eng.input(&A_sum.serialize());
         InputsHash::from_engine(eng)
     }
-    pub(crate) fn to_scalar(self) -> Scalar {
+
+    fn to_scalar(self) -> Scalar {
         // This is statistically extremely unlikely to panic.
         Scalar::from_be_bytes(self.to_byte_array()).expect("hash value greater than curve order")
     }
@@ -67,9 +65,6 @@ impl SharedSecretHash {
     }
 }
 
-pub(crate) fn calculate_input_hash(
-    outpoints_data: NonEmptyArray<OutPoint>,
-    A_sum: PublicKey,
-) -> Scalar {
-    InputsHash::from_outpoint_and_A_sum(outpoints_data.min(), A_sum).to_scalar()
+pub(crate) fn calculate_input_hash(smaller_outpoint: &OutPoint, A_sum: PublicKey) -> Scalar {
+    InputsHash::from_outpoint_and_A_sum(smaller_outpoint, A_sum).to_scalar()
 }
