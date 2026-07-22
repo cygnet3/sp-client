1a275b837800f21ec6d814da3fdecff0335718fc
Sosthene <contact@sosthene.net>
2026-07-22 12:58:15 +0200

    feat: add coin_select module

--- spdk-wallet/src/client/mod.rs
+++ spdk-wallet/src/client/mod.rs
@@ -1,8 +1,10 @@
 mod bip321_parsing;
 mod client;
+mod coin_select;
 mod spend;
 mod structs;
 
 pub use bip321_parsing::{SpUriExtension, SpUriParseError, parse_sp, parse_tsp};
 pub use client::SpClient;
+pub use coin_select::*;
 pub use structs::*;
