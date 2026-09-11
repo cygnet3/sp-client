cygnet3/spdk

refactor: replace `SharedSecret` with `TransactionSharedSecret` + introduce `PublicTweakData`

silentpayments/examples/find_output.rs
@@ -1,38 +1,28 @@
 use std::{env, error::Error, str::FromStr};
 
-// Import necessary libraries and modules
 use bip39::Mnemonic;
 use bitcoin::bip32::{DerivationPath, Xpriv};
 use bitcoin::consensus::deserialize;
-use bitcoin::secp256k1::{PublicKey, Secp256k1, XOnlyPublicKey};
+use bitcoin::secp256k1::Secp256k1 as BtcSecp256k1;
 use bitcoin::{Network, PrivateKey, ScriptBuf, Transaction};
 use bitcoin_hashes::hex::FromHex;
 
-use silentpayments::utils::{OutPoint, TEST_SCAN_PATH, TEST_SPEND_PATH};
-use silentpayments::SpVersion;
+use silentpayments::utils::{TEST_SCAN_PATH, TEST_SPEND_PATH};
 // Import types from the silentpayments library
 use silentpayments::receiving::{Label, Receiver};
-use silentpayments::utils::receiving::{
-    calculate_ecdh_shared_secret, calculate_tweak_data, get_pubkey_from_input,
-};
+use silentpayments::secp256k1::{Secp256k1, SecretKey, XOnlyPublicKey};
+use silentpayments::utils::receiving::{get_pubkey_from_input, PublicTweakData};
+use silentpayments::utils::OutPoint;
+use silentpayments::{Network as SpNetwork, SpVersion, TransactionInputs, TransactionSharedSecret};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Get the command-line arguments
     let args: Vec<String> = env::args().collect();
 
-    // Parse the mnemonic phrase from the first command-line argument
     let m = Mnemonic::from_str(args.get(1).unwrap())?;
-
-    // Get the transaction hex string from the second command-line argument
     let tx_hex = args.get(2).unwrap();
-
-    // Parse the scriptpubkeys from the third command-line argument, split by whitespace and store them in a vector
     let spks: Vec<&str> = args.get(3).unwrap().split_whitespace().collect();
 
-    // Deserialize the transaction hex string into a Transaction object
     let tx: Transaction = deserialize(Vec::from_hex(tx_hex)?.as_slice())?;
-
-    // Assert that the number of inputs in the transaction matches the number of scriptpubkeys provided
     assert!(tx.input.len() == spks.len());
 
     let master_key = Xpriv::new_master(bitcoin::Network::Signet, &m.to_seed(""))?;
@@ -41,58 +31,42 @@ fn main() -> Result<(), Box<dyn Error>> {
     let scan_path = DerivationPath::from_str(TEST_SCAN_PATH).unwrap();
     let spend_path = DerivationPath::from_str(TEST_SPEND_PATH).unwrap();
 
-    // Create a new instance of Secp256k1 for cryptographic operations
-    let secp = Secp256k1::signing_only();
+    let btc_secp = BtcSecp256k1::signing_only();
+    let secp = Secp256k1::new();
 
-    // Get the private keys for both scan and spend paths
-    let scan_privkey = master_key.derive_priv(&secp, &scan_path)?.private_key;
-    let spend_privkey = master_key.derive_priv(&secp, &spend_path)?.private_key;
+    let scan_privkey = master_key.derive_priv(&btc_secp, &scan_path)?.private_key;
+    let spend_privkey = master_key.derive_priv(&btc_secp, &spend_path)?.private_key;
+    let scan_sk = SecretKey::from_slice(&scan_privkey.secret_bytes())?;
+    let spend_sk = SecretKey::from_slice(&spend_privkey.secret_bytes())?;
 
-    // Create a change label for the wallet
-    let change_label = Label::new(scan_privkey, 0);
+    let change_label = Label::new(scan_sk, 0);
 
-    // Create a new Receiver object with the private and public keys, along with the change label
     let receiver = Receiver::new(
         SpVersion::ZERO,
-        scan_privkey.public_key(&secp),
-        spend_privkey.public_key(&secp),
+        scan_sk.public_key(&secp),
+        spend_sk.public_key(&secp),
         change_label,
-        silentpayments::Network::Testnet,
+        SpNetwork::Testnet,
     )?;
 
-    // Extract outpoints (previous transaction outputs) from the transaction inputs and store them in a vector
-    let outpoints: Vec<OutPoint> = tx
-        .input
-        .iter()
-        .map(|i| {
-            let prevout = i.previous_output;
-            OutPoint::from_txid_and_vout(prevout.txid.to_string(), prevout.vout).unwrap()
-        })
-        .collect();
-
-    // Iterate through each input of the transaction and assert if it contains an eligible pubkey
-    let mut input_pubkeys: Vec<PublicKey> = vec![];
+    let mut inputs = TransactionInputs::new();
     for (i, input) in tx.input.iter().enumerate() {
+        let prevout = &input.previous_output;
+        let outpoint =
+            OutPoint::from_txid_and_vout(prevout.txid.to_string(), prevout.vout).unwrap();
         let spk = ScriptBuf::from_hex(spks.get(i).unwrap())?;
-        if let Some(pubkey) = get_pubkey_from_input(
+        let pubkey = get_pubkey_from_input(
             input.script_sig.as_bytes(),
             &input.witness.to_vec(),
             spk.as_bytes(),
-        )? {
-            input_pubkeys.push(pubkey);
-        }
+        )?;
+        inputs.push(outpoint, spk.to_bytes(), pubkey);
     }
 
-    // Get the reference to a vector of public keys for further calculations
-    let pubkeys_ref: Vec<&PublicKey> = input_pubkeys.iter().collect();
+    let tweak_data = PublicTweakData::new(&secp, &inputs)?;
+    let ecdh_shared_secret =
+        TransactionSharedSecret::new_from_public_tweak_data(&secp, &tweak_data, &scan_sk)?;
 
-    // Calculate the tweak data based on the public keys and outpoints
-    let tweak_data = calculate_tweak_data(&pubkeys_ref, &outpoints)?;
-
-    // Calculate the ECDH shared secret between the scan private key and the tweak data
-    let ecdh_shared_secret = calculate_ecdh_shared_secret(&tweak_data, &scan_privkey);
-
-    // Filter the transaction outputs that have a valid P2TR scriptpubkey
     let pubkeys_to_check: Vec<_> = tx
         .output
         .iter()
@@ -103,16 +77,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .collect();
 
-    // Scan the transaction for eligible outputs and store them in a vector with their corresponding labels and key maps
     let my_outputs = receiver.scan_transaction(&ecdh_shared_secret, &pubkeys_to_check)?;
 
     println!("Found {} output(s)", my_outputs.len());
 
-    // Iterate through each found output and print the private key required to spend it along with its descriptor for importing into Bitcoin Core
     for (label, key_map) in my_outputs {
         println!("Found {} output(s) with label {:?}", key_map.len(), label);
         for (xonly, sk) in key_map {
-            let spending_key = spend_privkey.add_tweak(&sk).unwrap();
+            let spending_key = spend_sk.add_tweak(&sk)?;
             let wif = PrivateKey::from_slice(&spending_key.secret_bytes(), Network::Signet)
                 .unwrap()
                 .to_wif();

silentpayments/src/utils/sending.rs
@@ -1,11 +1,35 @@
 //! Sending utility functions.
-use crate::utils::common::{NonEmptyArray, OutPoint};
-use crate::{utils::common::SharedSecret, Error, Result};
+use crate::utils::common::{ecdh_multiply, NonEmptyArray, OutPoint, TransactionSharedSecret};
+use crate::{Error, Result};
 use secp256k1::constants::SECRET_KEY_SIZE;
-use secp256k1::ecdh::shared_secret_point;
-use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use secp256k1::{PublicKey, Secp256k1, SecretKey, Signing};
 
 use super::hash::calculate_input_hash;
+
+/// Guarantees that the secret key produces an even x-only public key when the spent output is taproot,
+/// by negating the secret key if necessary.
+#[derive(Debug, Clone)]
+pub struct NormalizedSecretKey(SecretKey);
+
+impl NormalizedSecretKey {
+    pub fn new<C: Signing>(secp: &Secp256k1<C>, secret_key: SecretKey, is_taproot: bool) -> Self {
+        let (_, parity) = secret_key.x_only_public_key(secp);
+
+        if is_taproot && parity == secp256k1::Parity::Odd {
+            return Self(secret_key.negate());
+        }
+
+        Self(secret_key)
+    }
+
+    pub fn as_inner(&self) -> &SecretKey {
+        &self.0
+    }
+
+    pub fn into_inner(self) -> SecretKey {
+        self.0
+    }
+}
 
 /// Represents the sum of all eligible input private keys of a transaction, multiplied with the input hash.
 #[derive(Clone, Copy, Debug)]
@@ -70,14 +94,11 @@ pub fn calculate_partial_secret(
 pub fn calculate_ecdh_shared_secret(
     B_scan: &PublicKey,
     partial_secret: &PartialSecret,
-) -> SharedSecret {
-    let mut ss_bytes = [0u8; 65];
-    ss_bytes[0] = 0x04;
-
-    // Using `shared_secret_point` to ensure the multiplication is constant time
-    ss_bytes[1..].copy_from_slice(&shared_secret_point(B_scan, &partial_secret.0));
-
-    SharedSecret(PublicKey::from_slice(&ss_bytes).expect("guaranteed to be a point on the curve"))
+) -> TransactionSharedSecret {
+    TransactionSharedSecret::from_sender_ecdh(
+        ecdh_multiply(B_scan, &partial_secret.0).expect("guaranteed to be a point on the curve"),
+        *B_scan,
+    )
 }
 
 fn get_a_sum_secret_keys(input: &[(SecretKey, bool)]) -> Result<SecretKey> {
@@ -85,25 +106,18 @@ fn get_a_sum_secret_keys(input: &[(SecretKey, bool)]) -> Result<SecretKey> {
         return Err(Error::GenericError("No input provided".to_owned()));
     }
 
-    let secp = secp256k1::Secp256k1::new();
+    let secp = Secp256k1::new();
 
-    let mut negated_keys: Vec<SecretKey> = vec![];
-
-    for (key, is_taproot) in input {
-        let (_, parity) = key.x_only_public_key(&secp);
-
-        if *is_taproot && parity == secp256k1::Parity::Odd {
-            negated_keys.push(key.negate());
-        } else {
-            negated_keys.push(*key);
-        }
-    }
-
-    let (head, tail) = negated_keys.split_first().expect("input is non-empty");
-
-    let result: SecretKey = tail
+    let normalized: Vec<NormalizedSecretKey> = input
         .iter()
-        .try_fold(*head, |acc, &item| acc.add_tweak(&item.into()))?;
+        .map(|(key, is_taproot)| NormalizedSecretKey::new(&secp, *key, *is_taproot))
+        .collect();
 
-    Ok(result)
+    let (head, tail) = normalized.split_first().expect("input is non-empty");
+
+    tail.iter()
+        .try_fold(*head.as_inner(), |acc, item| {
+            acc.add_tweak(&(*item.as_inner()).into())
+        })
+        .map_err(Error::from)
 }

silentpayments/tests/vector_tests.rs
@@ -2,17 +2,15 @@
 mod common;
 #[cfg(test)]
 mod tests {
-    use secp256k1::{PublicKey, Scalar, Secp256k1, SecretKey};
+    use secp256k1::{Scalar, Secp256k1, SecretKey};
     use silentpayments::{
         receiving::Label,
         utils::{
-            receiving::{
-                calculate_ecdh_shared_secret, calculate_tweak_data, get_pubkey_from_input, is_p2tr,
-            },
+            receiving::{get_pubkey_from_input, is_p2tr, PublicTweakData},
             sending::calculate_partial_secret,
             OutPoint,
         },
-        Network, SilentPaymentAddress,
+        Network, SilentPaymentAddress, TransactionInputs, TransactionSharedSecret,
     };
     use std::{collections::HashSet, io::Cursor, str::FromStr};
 
@@ -78,8 +76,6 @@ mod tests {
             // the wallet should make sure the amount sent are correct
             let silent_addresses = decode_recipients(&given.recipients);
 
-            // as an alternative, we could first multiply each input priv key with the input hash
-            // that way, we never expose the sk to our library
             let partial_secret = calculate_partial_secret(&input_priv_keys, &outpoints).unwrap();
             let outputs = generate_recipient_pubkeys(silent_addresses, partial_secret).unwrap();
 
@@ -116,30 +112,29 @@ mod tests {
 
             let outputs_to_check = decode_outputs_to_check(&given.outputs);
 
-            let outpoints: Vec<OutPoint> = given
-                .vin
-                .iter()
-                .map(|vin| OutPoint::from_txid_and_vout(vin.txid.clone(), vin.vout).unwrap())
-                .collect();
-            let mut input_pub_keys = Vec::new();
+            let mut inputs = TransactionInputs::new();
             for input in given.vin {
                 let script_sig = hex::decode(&input.scriptSig).unwrap();
                 let txinwitness_bytes = hex::decode(&input.txinwitness).unwrap();
                 let mut cursor = Cursor::new(&txinwitness_bytes);
                 let txinwitness = deser_string_vector(&mut cursor).unwrap();
                 let script_pub_key = hex::decode(&input.prevout.scriptPubKey.hex).unwrap();
+                let outpoint =
+                    OutPoint::from_txid_and_vout(input.txid.clone(), input.vout).unwrap();
 
                 match get_pubkey_from_input(&script_sig, &txinwitness, &script_pub_key) {
-                    Ok(Some(pubkey)) => input_pub_keys.push(pubkey),
-                    Ok(None) => (),
+                    Ok(Some(pubkey)) => {
+                        inputs.push(outpoint, script_pub_key, Some(pubkey));
+                    }
+                    Ok(None) => {
+                        inputs.push(outpoint, script_pub_key, None);
+                    }
                     Err(e) => panic!("Problem parsing the input: {:?}", e),
                 }
             }
-            if input_pub_keys.is_empty() {
+            if inputs.input_pubkeys().iter().all(|pk| pk.is_none()) {
                 continue;
-            };
-
-            let input_pub_keys: Vec<&PublicKey> = input_pub_keys.iter().collect();
+            }
 
             for label_int in &given.labels {
                 let label = Label::new(b_scan, *label_int);
@@ -168,8 +163,10 @@ mod tests {
             // to the expected addresses
             assert_eq!(set1, set2);
 
-            let tweak_data = calculate_tweak_data(&input_pub_keys, &outpoints).unwrap();
-            let ecdh_shared_secret = calculate_ecdh_shared_secret(&tweak_data, &b_scan);
+            let tweak_data = PublicTweakData::new(&secp, &inputs).unwrap();
+            let ecdh_shared_secret =
+                TransactionSharedSecret::new_from_public_tweak_data(&secp, &tweak_data, &b_scan)
+                    .unwrap();
 
             let scanned_outputs_received = sp_receiver
                 .scan_transaction(&ecdh_shared_secret, &outputs_to_check)

cygnet3/sp-client

silentpayments/examples/find_output.rs
@@ -1,38 +1,28 @@
 use std::{env, error::Error, str::FromStr};
 
-// Import necessary libraries and modules
 use bip39::Mnemonic;
 use bitcoin::bip32::{DerivationPath, Xpriv};
 use bitcoin::consensus::deserialize;
-use bitcoin::secp256k1::{PublicKey, Secp256k1, XOnlyPublicKey};
+use bitcoin::secp256k1::Secp256k1 as BtcSecp256k1;
 use bitcoin::{Network, PrivateKey, ScriptBuf, Transaction};
 use bitcoin_hashes::hex::FromHex;
 
-use silentpayments::SpVersion;
-use silentpayments::utils::{OutPoint, TEST_SCAN_PATH, TEST_SPEND_PATH};
+use silentpayments::utils::{TEST_SCAN_PATH, TEST_SPEND_PATH};
 // Import types from the silentpayments library
 use silentpayments::receiving::{Label, Receiver};
-use silentpayments::utils::receiving::{
-    calculate_ecdh_shared_secret, calculate_tweak_data, get_pubkey_from_input,
-};
+use silentpayments::secp256k1::{Secp256k1, SecretKey, XOnlyPublicKey};
+use silentpayments::utils::OutPoint;
+use silentpayments::utils::receiving::{PublicTweakData, get_pubkey_from_input};
+use silentpayments::{Network as SpNetwork, SpVersion, TransactionInputs, TransactionSharedSecret};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Get the command-line arguments
     let args: Vec<String> = env::args().collect();
 
-    // Parse the mnemonic phrase from the first command-line argument
     let m = Mnemonic::from_str(args.get(1).unwrap())?;
-
-    // Get the transaction hex string from the second command-line argument
     let tx_hex = args.get(2).unwrap();
-
-    // Parse the scriptpubkeys from the third command-line argument, split by whitespace and store them in a vector
     let spks: Vec<&str> = args.get(3).unwrap().split_whitespace().collect();
 
-    // Deserialize the transaction hex string into a Transaction object
     let tx: Transaction = deserialize(Vec::from_hex(tx_hex)?.as_slice())?;
-
-    // Assert that the number of inputs in the transaction matches the number of scriptpubkeys provided
     assert!(tx.input.len() == spks.len());
 
     let master_key = Xpriv::new_master(bitcoin::Network::Signet, &m.to_seed(""))?;
@@ -41,58 +31,42 @@ fn main() -> Result<(), Box<dyn Error>> {
     let scan_path = DerivationPath::from_str(TEST_SCAN_PATH).unwrap();
     let spend_path = DerivationPath::from_str(TEST_SPEND_PATH).unwrap();
 
-    // Create a new instance of Secp256k1 for cryptographic operations
-    let secp = Secp256k1::signing_only();
+    let btc_secp = BtcSecp256k1::signing_only();
+    let secp = Secp256k1::new();
 
-    // Get the private keys for both scan and spend paths
-    let scan_privkey = master_key.derive_priv(&secp, &scan_path)?.private_key;
-    let spend_privkey = master_key.derive_priv(&secp, &spend_path)?.private_key;
+    let scan_privkey = master_key.derive_priv(&btc_secp, &scan_path)?.private_key;
+    let spend_privkey = master_key.derive_priv(&btc_secp, &spend_path)?.private_key;
+    let scan_sk = SecretKey::from_slice(&scan_privkey.secret_bytes())?;
+    let spend_sk = SecretKey::from_slice(&spend_privkey.secret_bytes())?;
 
-    // Create a change label for the wallet
-    let change_label = Label::new(scan_privkey, 0);
+    let change_label = Label::new(scan_sk, 0);
 
-    // Create a new Receiver object with the private and public keys, along with the change label
     let receiver = Receiver::new(
         SpVersion::ZERO,
-        scan_privkey.public_key(&secp),
-        spend_privkey.public_key(&secp),
+        scan_sk.public_key(&secp),
+        spend_sk.public_key(&secp),
         change_label,
-        silentpayments::Network::Testnet,
+        SpNetwork::Testnet,
     )?;
 
-    // Extract outpoints (previous transaction outputs) from the transaction inputs and store them in a vector
-    let outpoints: Vec<OutPoint> = tx
-        .input
-        .iter()
-        .map(|i| {
-            let prevout = i.previous_output;
-            OutPoint::from_txid_and_vout(prevout.txid.to_string(), prevout.vout).unwrap()
-        })
-        .collect();
-
-    // Iterate through each input of the transaction and assert if it contains an eligible pubkey
-    let mut input_pubkeys: Vec<PublicKey> = vec![];
+    let mut inputs = TransactionInputs::new();
     for (i, input) in tx.input.iter().enumerate() {
+        let prevout = &input.previous_output;
+        let outpoint =
+            OutPoint::from_txid_and_vout(prevout.txid.to_string(), prevout.vout).unwrap();
         let spk = ScriptBuf::from_hex(spks.get(i).unwrap())?;
-        if let Some(pubkey) = get_pubkey_from_input(
+        let pubkey = get_pubkey_from_input(
             input.script_sig.as_bytes(),
             &input.witness.to_vec(),
             spk.as_bytes(),
-        )? {
-            input_pubkeys.push(pubkey);
-        }
+        )?;
+        inputs.push(outpoint, spk.to_bytes(), pubkey);
     }
 
-    // Get the reference to a vector of public keys for further calculations
-    let pubkeys_ref: Vec<&PublicKey> = input_pubkeys.iter().collect();
+    let tweak_data = PublicTweakData::new(&secp, &inputs)?;
+    let ecdh_shared_secret =
+        TransactionSharedSecret::new_from_public_tweak_data(&secp, &tweak_data, &scan_sk)?;
 
-    // Calculate the tweak data based on the public keys and outpoints
-    let tweak_data = calculate_tweak_data(&pubkeys_ref, &outpoints)?;
-
-    // Calculate the ECDH shared secret between the scan private key and the tweak data
-    let ecdh_shared_secret = calculate_ecdh_shared_secret(&tweak_data, &scan_privkey);
-
-    // Filter the transaction outputs that have a valid P2TR scriptpubkey
     let pubkeys_to_check: Vec<_> = tx
         .output
         .iter()
@@ -103,16 +77,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .collect();
 
-    // Scan the transaction for eligible outputs and store them in a vector with their corresponding labels and key maps
     let my_outputs = receiver.scan_transaction(&ecdh_shared_secret, &pubkeys_to_check)?;
 
     println!("Found {} output(s)", my_outputs.len());
 
-    // Iterate through each found output and print the private key required to spend it along with its descriptor for importing into Bitcoin Core
     for (label, key_map) in my_outputs {
         println!("Found {} output(s) with label {:?}", key_map.len(), label);
         for (xonly, sk) in key_map {
-            let spending_key = spend_privkey.add_tweak(&sk).unwrap();
+            let spending_key = spend_sk.add_tweak(&sk)?;
             let wif = PrivateKey::from_slice(&spending_key.secret_bytes(), Network::Signet)
                 .unwrap()
                 .to_wif();

silentpayments/src/utils/sending.rs
@@ -1,11 +1,35 @@
 //! Sending utility functions.
-use crate::utils::common::{NonEmptyArray, OutPoint};
-use crate::{Error, Result, utils::common::SharedSecret};
+use crate::utils::common::{NonEmptyArray, OutPoint, TransactionSharedSecret, ecdh_multiply};
+use crate::{Error, Result};
 use secp256k1::constants::SECRET_KEY_SIZE;
-use secp256k1::ecdh::shared_secret_point;
-use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use secp256k1::{PublicKey, Secp256k1, SecretKey, Signing};
 
 use super::hash::calculate_input_hash;
+
+/// Guarantees that the secret key produces an even x-only public key when the spent output is taproot,
+/// by negating the secret key if necessary.
+#[derive(Debug, Clone)]
+pub struct NormalizedSecretKey(SecretKey);
+
+impl NormalizedSecretKey {
+    pub fn new<C: Signing>(secp: &Secp256k1<C>, secret_key: SecretKey, is_taproot: bool) -> Self {
+        let (_, parity) = secret_key.x_only_public_key(secp);
+
+        if is_taproot && parity == secp256k1::Parity::Odd {
+            return Self(secret_key.negate());
+        }
+
+        Self(secret_key)
+    }
+
+    pub fn as_inner(&self) -> &SecretKey {
+        &self.0
+    }
+
+    pub fn into_inner(self) -> SecretKey {
+        self.0
+    }
+}
 
 /// Represents the sum of all eligible input private keys of a transaction, multiplied with the input hash.
 #[derive(Clone, Copy, Debug)]
@@ -70,14 +94,11 @@ pub fn calculate_partial_secret(
 pub fn calculate_ecdh_shared_secret(
     B_scan: &PublicKey,
     partial_secret: &PartialSecret,
-) -> SharedSecret {
-    let mut ss_bytes = [0u8; 65];
-    ss_bytes[0] = 0x04;
-
-    // Using `shared_secret_point` to ensure the multiplication is constant time
-    ss_bytes[1..].copy_from_slice(&shared_secret_point(B_scan, &partial_secret.0));
-
-    SharedSecret(PublicKey::from_slice(&ss_bytes).expect("guaranteed to be a point on the curve"))
+) -> TransactionSharedSecret {
+    TransactionSharedSecret::from_sender_ecdh(
+        ecdh_multiply(B_scan, &partial_secret.0).expect("guaranteed to be a point on the curve"),
+        *B_scan,
+    )
 }
 
 fn get_a_sum_secret_keys(input: &[(SecretKey, bool)]) -> Result<SecretKey> {
@@ -85,25 +106,18 @@ fn get_a_sum_secret_keys(input: &[(SecretKey, bool)]) -> Result<SecretKey> {
         return Err(Error::GenericError("No input provided".to_owned()));
     }
 
-    let secp = secp256k1::Secp256k1::new();
+    let secp = Secp256k1::new();
 
-    let mut negated_keys: Vec<SecretKey> = vec![];
-
-    for (key, is_taproot) in input {
-        let (_, parity) = key.x_only_public_key(&secp);
-
-        if *is_taproot && parity == secp256k1::Parity::Odd {
-            negated_keys.push(key.negate());
-        } else {
-            negated_keys.push(*key);
-        }
-    }
-
-    let (head, tail) = negated_keys.split_first().expect("input is non-empty");
-
-    let result: SecretKey = tail
+    let normalized: Vec<NormalizedSecretKey> = input
         .iter()
-        .try_fold(*head, |acc, &item| acc.add_tweak(&item.into()))?;
+        .map(|(key, is_taproot)| NormalizedSecretKey::new(&secp, *key, *is_taproot))
+        .collect();
 
-    Ok(result)
+    let (head, tail) = normalized.split_first().expect("input is non-empty");
+
+    tail.iter()
+        .try_fold(*head.as_inner(), |acc, item| {
+            acc.add_tweak(&(*item.as_inner()).into())
+        })
+        .map_err(Error::from)
 }

silentpayments/tests/vector_tests.rs
@@ -2,15 +2,13 @@
 mod common;
 #[cfg(test)]
 mod tests {
-    use secp256k1::{PublicKey, Scalar, Secp256k1, SecretKey};
+    use secp256k1::{Scalar, Secp256k1, SecretKey};
     use silentpayments::{
-        Network, SilentPaymentCode,
+        Network, SilentPaymentCode, TransactionInputs, TransactionSharedSecret,
         receiving::Label,
         utils::{
             OutPoint,
-            receiving::{
-                calculate_ecdh_shared_secret, calculate_tweak_data, get_pubkey_from_input, is_p2tr,
-            },
+            receiving::{PublicTweakData, get_pubkey_from_input, is_p2tr},
             sending::calculate_partial_secret,
         },
     };
@@ -78,8 +76,6 @@ mod tests {
             // the wallet should make sure the amount sent are correct
             let silent_payment_codes = decode_recipients(&given.recipients);
 
-            // as an alternative, we could first multiply each input priv key with the input hash
-            // that way, we never expose the sk to our library
             let partial_secret = calculate_partial_secret(&input_priv_keys, &outpoints).unwrap();
             let outputs = generate_recipient_pubkeys(silent_payment_codes, partial_secret).unwrap();
 
@@ -116,30 +112,29 @@ mod tests {
 
             let outputs_to_check = decode_outputs_to_check(&given.outputs);
 
-            let outpoints: Vec<OutPoint> = given
-                .vin
-                .iter()
-                .map(|vin| OutPoint::from_txid_and_vout(vin.txid.clone(), vin.vout).unwrap())
-                .collect();
-            let mut input_pub_keys = Vec::new();
+            let mut inputs = TransactionInputs::new();
             for input in given.vin {
                 let script_sig = hex::decode(&input.scriptSig).unwrap();
                 let txinwitness_bytes = hex::decode(&input.txinwitness).unwrap();
                 let mut cursor = Cursor::new(&txinwitness_bytes);
                 let txinwitness = deser_string_vector(&mut cursor).unwrap();
                 let script_pub_key = hex::decode(&input.prevout.scriptPubKey.hex).unwrap();
+                let outpoint =
+                    OutPoint::from_txid_and_vout(input.txid.clone(), input.vout).unwrap();
 
                 match get_pubkey_from_input(&script_sig, &txinwitness, &script_pub_key) {
-                    Ok(Some(pubkey)) => input_pub_keys.push(pubkey),
-                    Ok(None) => (),
+                    Ok(Some(pubkey)) => {
+                        inputs.push(outpoint, script_pub_key, Some(pubkey));
+                    }
+                    Ok(None) => {
+                        inputs.push(outpoint, script_pub_key, None);
+                    }
                     Err(e) => panic!("Problem parsing the input: {:?}", e),
                 }
             }
-            if input_pub_keys.is_empty() {
+            if inputs.input_pubkeys().iter().all(|pk| pk.is_none()) {
                 continue;
-            };
-
-            let input_pub_keys: Vec<&PublicKey> = input_pub_keys.iter().collect();
+            }
 
             for label_int in &given.labels {
                 let label = Label::new(b_scan, *label_int);
@@ -166,8 +161,10 @@ mod tests {
             // to the expected codes
             assert_eq!(receiving_codes, expected_codes);
 
-            let tweak_data = calculate_tweak_data(&input_pub_keys, &outpoints).unwrap();
-            let ecdh_shared_secret = calculate_ecdh_shared_secret(&tweak_data, &b_scan);
+            let tweak_data = PublicTweakData::new(&secp, &inputs).unwrap();
+            let ecdh_shared_secret =
+                TransactionSharedSecret::new_from_public_tweak_data(&secp, &tweak_data, &b_scan)
+                    .unwrap();
 
             let scanned_outputs_received = sp_receiver
                 .scan_transaction(&ecdh_shared_secret, &outputs_to_check)